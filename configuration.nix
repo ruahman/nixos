3a38f8a7c56f221af0407e1c26bf2791652ce06e
Diego R Vila <dego_vila@yahoo.com>
2024-12-22 00:36:42 -0400

    basic config

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,11 @@
   services.xserver.xkb = {
     layout = "us";
     variant = "";
+    options = "caps:ctrl_modifier";
   };
+
+  # enable console to use xkb settings
+  console.useXkbConfig = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -96,7 +100,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim 
     git
   ];
 
