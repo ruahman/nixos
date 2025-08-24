cf9d2254849adacf2c00b4a09aafa8ece9387779
Diego R Vila <dego_vila@yahoo.com>
2025-08-24 18:06:00 -0400

    kitty back

--- home.nix
+++ home.nix
@@ -27,12 +27,11 @@
     libreoffice
     
     # typst
-    typst
+    #typst
 
     # notes
-    obsidian
+    #obsidian
     anytype
-    #notion-app
 
     # text editors
     neovim
@@ -44,11 +43,9 @@
     ghostty
     kitty
     terminator
-    termite
 
     # browsers
     google-chrome
-    ladybird
 
     #db
     sqlitebrowser
@@ -208,100 +205,12 @@
     };
   };
 
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "FiraCode Nerd Font";
-      size = 20.0;
-    };
-    settings = {
-      #shell = "${pkgs.nushell}/bin/nu";
-
-      # basic colors
-      foreground =  "#c6d0f5";
-      background =  "#303446";
-      background_opacity = 0.9;
-      selection_foreground = "#303446";
-      selection_background = "#f2d5cf";
-
-      # Cursor colors
-      cursor = "#f2d5cf";
-      cursor_text_color = "#303446";
-
-      # URL underline color when hovering with mouse
-      url_color = "#f2d5cf";
-      
-      # Kitty window border colors
-      active_border_color = "#babbf1";
-      inactive_border_color = "#737994";
-      bell_border_color = "#e5c890";
-
-      # OS Window titlebar colors
-      wayland_titlebar_color = "system";
-      macos_titlebar_color =  "system";
-
-      # Tab bar colors
-      active_tab_foreground = "#232634";
-      active_tab_background = "#ca9ee6";
-      inactive_tab_foreground = "#c6d0f5";
-      inactive_tab_background = "#292c3c";
-      tab_bar_background = "#232634";
-
-      # Colors for marks (marked text in the terminal)
-      mark1_foreground = "#303446";
-      mark1_background = "#babbf1";
-      mark2_foreground = "#303446";
-      mark2_background = "#ca9ee6";
-      mark3_foreground = "#303446";
-      mark3_background = "#85c1dc";
-
-      # The 16 terminal colors
-      
-      # black
-      color0 = "#51576d";
-      color8 = "#626880";
-      
-      # red
-      color1 = "#e78284";
-      color9 = "#e78284";
-      
-      # green
-      color2 =  "#a6d189";
-      color10 = "#a6d189";
-      
-      # yellow
-      color3 =  "#e5c890";
-      color11 = "#e5c890";
-      
-      # blue
-      color4 =  "#8caaee";
-      color12 = "#8caaee";
-      
-      # magenta
-      color5 =  "#f4b8e4";
-      color13 = "#f4b8e4";
-      
-      # cyan
-      color6 =  "#81c8be";
-      color14 = "#81c8be";
-      
-      # white
-      color7 =  "#b5bfe2";
-      color15 = "#a5adce";
-    };
-  };
-
   programs.terminator = {
     enable = true;
     config = {
       profiles.default.use_system_font = false;
       profiles.default.font = "Terminess Nerd Font 20";
     };
-  };
-
-  programs.termite = {
-    enable = true;
-    font = "Hack Nerd Font 20";
   };
 
   programs.carapace = {
@@ -362,6 +271,7 @@
       set -g mouse on
       set-option -g status-position top
       set -g base-index 1
+      bind-key -n C-Space run-shell "clear && tmux clear-history"
     ''; 
   };
   
