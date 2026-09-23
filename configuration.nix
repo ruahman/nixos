e7919e5aed21ce358b2e4ac47ee6fec1b3df0287
Diego R Vila <dego_vila@yahoo.com>
2026-09-23 12:32:59 -0400

    unetbootin

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    unetbootin
     podman
     podman-compose
     docker 
